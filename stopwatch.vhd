9ce5412573f4d8eabc12b1edaa63f93132fa5c34
Retr0-code <nikk3695@gmail.com>
2025-10-14 14:58:09 +0300

    Added untested BCD register
    Added generic parameters for timer
    Updated stopwatch accoring to changes in components

--- stopwatch.vhd
+++ stopwatch.vhd
@@ -3,47 +3,57 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity stopwatch is
-  generic (
-    g_clock_divider : integer := 5000000  -- Passing generic to timer component
-  );
-  port (
-    i_clk        : in std_logic; -- Clock input
-    i_start_stop : in std_logic  -- Button input
-  );
+	generic (
+		g_clock_divider	: integer := 5000000; -- If 5 000 000 periods passed for 50 MHz clock then 100 ms added to time register
+		g_divider_nbit		: integer := 23; -- Default amount of bits for 5 000 000 periods accumulation
+		g_timer_nbit		: integer := 14; -- Minimal amount of bits to store max of 16384 by 100ms
+		g_digits_amount	: integer := 4
+	);
+	port (
+		i_clk        : in std_logic; -- Clock input
+		i_start_stop : in std_logic  -- Button input
+	);
 end entity stopwatch;
 
 architecture structural of stopwatch is
 	component timer
 		generic (
-			g_clock_divider : integer := 5000000 -- If 5 000 000 periods passed for 50 MHz clock then 100 ms added to time register
+			g_clock_divider	: integer := 5000000; -- If 5 000 000 periods passed for 50 MHz clock then 100 ms added to time register
+			g_divider_nbit		: integer := 23; -- Default amount of bits for 5 000 000 periods accumulation
+			g_timer_nbit		: integer := 14 -- Minimal amount of bits to store max of 16384 by 100ms
 		);
 		
 		port (
 			i_clk				: in std_logic; -- Assuming clock frequency is 50 MHz
 			i_start_stop	: in std_logic; -- A signal pulse to set T-flipflop
-			o_time			: out unsigned(15 downto 0) -- Amount of time in 100 ms
+			o_time			: out unsigned(g_timer_nbit - 1 downto 0) -- Amount of time in 100 ms
 		);
 	end component;
-	
-	signal elapsed_time : unsigned(15 downto 0);
 	
 	component bcd_register
+		generic (
+			g_digits_amount : integer := 4
+		);
+
 		port (
 			i_clk	: in std_logic;
-			i_num	: in std_logic_vector(15 downto 0);
-			o_digit0, o_digit1, o_digit2, o_digit3 : out std_logic_vector(4 downto 0)
+			i_num	: in std_logic_vector(g_digits_amount * 4 - 1 downto 0);
+			o_digits	: out std_logic_vector(g_digits_amount * 4 - 1 downto 0)
 		);
 	end component;
 	
-	signal o_digit0, o_digit1, o_digit2, o_digit3 : std_logic_vector(4 downto 0);
+	signal w_output_digits : std_logic_vector(g_digits_amount * 4 - 1 downto 0);
+	signal w_elapsed_time : unsigned(g_timer_nbit - 1 downto 0);
+	signal w_elapsed_time_converted : unsigned(g_digits_amount * 4 - 1 downto 0);
 	
 begin
 
+	w_elapsed_time_converted <= "00" & w_elapsed_time;
+
 	u_timer : timer
-		generic map (g_clock_divider)
-		port map (i_clk, i_start_stop, elapsed_time);
+		port map (i_clk, i_start_stop, w_elapsed_time);
 		
 	u_bcd_register : bcd_register
-		port map (i_clk, std_logic_vector(elapsed_time), o_digit0, o_digit1, o_digit2, o_digit3);
+		port map (i_clk, std_logic_vector(w_elapsed_time_converted), w_output_digits);
 
 end architecture structural;
--- timer.vhd
+++ timer.vhd
@@ -4,20 +4,22 @@ use ieee.numeric_std.all;
 
 entity timer is
 	generic (
-		g_clock_divider : integer := 5000000 -- If 5 000 000 periods passed for 50 MHz clock then 100 ms added to time register
+		g_clock_divider	: integer := 5000000; -- If 5 000 000 periods passed for 50 MHz clock then 100 ms added to time register
+		g_divider_nbit		: integer := 23; -- Default amount of bits for 5 000 000 periods accumulation
+		g_timer_nbit		: integer := 14 -- Minimal amount of bits to store max of 16384 by 100ms
 	);
 
 	port (
 		i_clk				: in std_logic; -- Assuming clock frequency is 50 MHz
 		i_start_stop	: in std_logic; -- A signal pulse to set T-flipflop
-		o_time			: out unsigned(15 downto 0) -- Amount of time in 100 ms
+		o_time			: out unsigned(g_timer_nbit - 1 downto 0) -- Amount of time in 100 ms
 	);
 end entity timer;
 
 architecture behavioral of timer is
 	signal r_start : std_logic := '0';
-	signal r_time	: unsigned(15 downto 0) := (others => '0');
-	signal r_period_counter	: unsigned(23 downto 0) := (others => '0');
+	signal r_time	: unsigned(g_timer_nbit - 1 downto 0) := (others => '0');
+	signal r_period_counter	: unsigned(g_divider_nbit - 1 downto 0) := (others => '0');
 begin
 
 	-- Period counter & timer process
