2c7c80bd2aa7ea6b9734636b90f4811a81fa9ef5
Retr0-code <nikk3695@gmail.com>
2025-10-18 00:44:40 +0300

    Fixed errors in syntax of select statement

--- dec2to4.vhd
+++ dec2to4.vhd
@@ -2,17 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity dec2to4 is
-	port (
-		i_counter : in std_logic_vector(1 downto 0);
-		o_decoder : out std_logic_vector(3 downto 0);
-	);
+    port (
+        i_counter : in std_logic_vector(1 downto 0);
+        o_decoder : out std_logic_vector(3 downto 0)
+    );
 end entity dec2to4;
 
 architecture dataflow of dec2to4 is
 begin
-	select i_counter
-		o_decoder	<= "0001" when "00"
-						<= "0010" when "01"
-						<= "0100" when "10"
-						<= "1000" when others;
+    with i_counter select
+        o_decoder <= "0001" when "00",
+                     "0010" when "01",
+                     "0100" when "10",
+                     "1000" when "11",
+                     "0000" when others;
 end architecture dataflow;
